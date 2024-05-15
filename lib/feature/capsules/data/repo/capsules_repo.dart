0b877cee618abce3bac55602fb3d2fbd7e464354
habibagamal123 <habiba.gamaall23@gmail.com>
2024-05-15 11:02:59 +0300

    CapsulesRepo

---
+++ lib/feature/capsules/data/repo/capsules_repo.dart
@@ -0,0 +1,23 @@
+
+import 'package:space_x/core/networking/api_error_handler.dart';
+import 'package:space_x/core/networking/api_result.dart';
+import 'package:space_x/feature/capsules/data/model/capsule.dart';
+import 'package:space_x/feature/home/data/data_sources/home_data_sources.dart';
+
+class CapsulesRepo {
+  final HomeRemoteDataSources _homeRemoteDataSources;
+
+  const CapsulesRepo({required HomeRemoteDataSources homeRemoteDataSources})
+      : _homeRemoteDataSources = homeRemoteDataSources;
+
+  Future<ApiResult<List<Capsule>>> fetchCapsules() async {
+    try {
+      
+    final capsules = await _homeRemoteDataSources.fetchCapsules();
+
+      return ApiResult.success(capsules);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+}
