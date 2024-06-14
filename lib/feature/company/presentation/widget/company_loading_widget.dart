9b0bdec48c337dd823e3fa5705049dcabd01d137
LAPTOP-KFFSGVHG\user <colonal.ch@gmail.com>
2024-06-14 17:06:35 +0300

    Refactor CompanyLoadingWidget layout to be responsive

--- lib/feature/company/presentation/widget/company_loading_widget.dart
+++ lib/feature/company/presentation/widget/company_loading_widget.dart
@@ -10,30 +10,39 @@ class CompanyLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const CompanyHeader(),
-          const CompanyLogo(),
-          const SizedBox(height: 30),
-          Expanded(
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: FittedBox(
-                fit: BoxFit.scaleDown,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 20),
-                  child: Image.asset(
-                    AssetsImages.animationAstronaut,
-                    fit: BoxFit.fitHeight,
-                    alignment: Alignment.topCenter,
-                    height: 200,
+      body: LayoutBuilder(builder: (context, constraints) {
+        return SizedBox(
+          width: constraints.maxWidth,
+          height: constraints.maxHeight,
+          child: Column(
+            children: [
+              const CompanyHeader(),
+              const CompanyLogo(),
+              const SizedBox(height: 30),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SizedBox(
+                    width: constraints.maxWidth,
+                    child: FittedBox(
+                      fit: BoxFit.scaleDown,
+                      child: Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: Image.asset(
+                          AssetsImages.animationAstronaut,
+                          fit: BoxFit.fitHeight,
+                          alignment: Alignment.topCenter,
+                          height: 200,
+                        ),
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        );
+      }),
     );
   }
 }
